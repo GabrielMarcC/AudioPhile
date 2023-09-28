26e71bb6e900dd61f87112c935c0e3926763db73
GabrielM <gabrielmarcelino099@gmail.com>
2023-09-28 17:56:49 -0300

    fix: change filterGallery

--- src/utils/filterGallery.ts
+++ src/utils/filterGallery.ts
@@ -5,23 +5,13 @@ export const filterGallery = (data: IGallery[], name: string) => {
     throw new Error("Failed to filter gallery");
   }
 
-  let objGallery = {} as IGallery;
+  let objGallery = { ...(data as {}) } as IGallery;
 
-  const gallery = data
-    .filter((item) => item.name === name)
-    .map(
-      (item) =>
-        (objGallery = {
-          category: item.category,
-          name: item.name,
-          img_1: item.img_1,
-          img_2: item.img_2,
-          img_3: item.img_3,
-          img_product: item.img_product,
-          no_bg_img: item.no_bg_img,
-          img_tablet: item.img_tablet,
-        })
-    );
+  const gallery = data.find((item) => {
+    return item.name === name;
+  });
+
+  if (gallery) objGallery = { ...gallery };
 
   return objGallery;
 };
