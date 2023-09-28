fdef2d93b692d26cb4b098e911f54d7818252f39
GabrielM <gabrielmarcelino099@gmail.com>
2023-09-28 17:37:33 -0300

    fix: filterGallery

--- src/utils/filterGallery.ts
+++ src/utils/filterGallery.ts
@@ -1,50 +1,27 @@
-// import { IGallery } from "@/types";
-
-// export const filterGallery = (data: IGallery[], name: string) => {
-//   if (!data && !name) {
-//     throw new Error("Failed to filter gallery");
-//   }
-
-//   let objGallery = {} as IGallery;
-
-//   const gallery = data
-//     .filter((item) => item.name === name)
-//     .map(
-//       (item) =>
-//         (objGallery = {
-//           category: item.category,
-//           name: item.name,
-//           img_1: item.img_1,
-//           img_2: item.img_2,
-//           img_3: item.img_3,
-//           img_product: item.img_product,
-//           no_bg_img: item.no_bg_img,
-//         })
-//     );
-
-//   return objGallery;
-// };
-
 import { IGallery } from "@/types";
 
 export const filterGallery = (data: IGallery[], name: string) => {
-  const foundItem = data.find((item) => item.name === name);
-
-  if (!foundItem) {
-    throw new Error("Gallery item not found");
+  if (!data && !name) {
+    throw new Error("Failed to filter gallery");
   }
 
-  const { category, img_1, img_2, img_3, img_product, no_bg_img, img_tablet } =
-    foundItem;
+  let objGallery = {} as IGallery;
 
-  return {
-    category,
-    name,
-    img_1,
-    img_2,
-    img_3,
-    img_product,
-    img_tablet,
-    no_bg_img,
-  };
+  const gallery = data
+    .filter((item) => item.name === name)
+    .map(
+      (item) =>
+        (objGallery = {
+          category: item.category,
+          name: item.name,
+          img_1: item.img_1,
+          img_2: item.img_2,
+          img_3: item.img_3,
+          img_product: item.img_product,
+          no_bg_img: item.no_bg_img,
+          img_tablet: item.img_tablet,
+        })
+    );
+
+  return objGallery;
 };
